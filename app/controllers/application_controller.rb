261b297abeaffefebdf3ee310a32f5a840505a11
jaxzzxaj <programyourlife1020@gmail.com>
2022-09-29 12:30:24 +0900

    ログイン時に取得したいkeyの設定
    ＋新規登録時に取得したいkeyの修正
    
    →ところどころカンマがドットになっていたから修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :get_parameters_from_devise, if: :devise_controller?
 
   def get_parameters_from_devise
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname. :name, :ruby_name, :family_name, :ruby_family_name, :birthday. :email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :name, :ruby_name, :family_name, :ruby_family_name, :birthday, :email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :password])
   end
 end

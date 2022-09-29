ad4d7676edba8465452d1ece70333834590c89b6
jaxzzxaj <programyourlife1020@gmail.com>
2022-09-29 14:22:44 +0900

    product_tableのカラム追加
    rails db:migrate後にエラーが発生
    
    [エラー]
    referenceがおかしいよ。referenceの"s"を追加後解決。

--- db/migrate/20220929050347_create_products.rb
+++ db/migrate/20220929050347_create_products.rb
@@ -1,7 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :charges_id, null:false
+      t.integer :estimated_shipping_date_id, null: false
+      t.string :name, null: false
+      t.integer :image, null: false
+      t.integer :price, null: false
+      t.string :text, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
